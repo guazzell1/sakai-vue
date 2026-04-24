6f30df0ddd628173fdf66228475907ea23ca6d84
Murilo <silveiragmurilo@gmail.com>
2026-04-23 21:01:24 -0300

    final

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
@@ -10,12 +11,30 @@ import ToastService from 'primevue/toastservice';
 import '@/assets/tailwind.css';
 import '@/assets/styles.scss';
 
+const UzumakiPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '#fff7ed',
+            100: '#ffedd5',
+            200: '#fed7aa',
+            300: '#fdba74',
+            400: '#fb923c',
+            500: '#f97316',
+            600: '#ea580c',
+            700: '#c2410c',
+            800: '#9a3412',
+            900: '#7c2d12',
+            950: '#431407'
+        }
+    }
+});
+
 const app = createApp(App);
 
 app.use(router);
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: UzumakiPreset,
         options: {
             darkModeSelector: '.app-dark'
         }
